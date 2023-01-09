838a27b018888695c1faba5381294bc76694dd18
Mohammed Abutabikh <>
2023-01-09 22:07:31 +0200

    initial commit

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Validation\UnauthorizedException;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+        abort(403, 'Unauthorized action.');
+    }
+}
